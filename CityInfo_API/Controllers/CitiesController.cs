0ad584ba9685b5f5d77fbe1bb9412030b154719c
mohsen zamani <mohsen.1408@gmail.com>
2025-04-10 00:33:30 +0330

    API Project
    API Project For Test and Learn API with ASP Core

---
+++ CityInfo_API/Controllers/CitiesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo_API.Controllers
+{
+    [ApiController]
+    // route 3: [Route("api/cities")]
+    // route 2: [Route("api/[controller]")]
+    public class CitiesController : Controller
+    {
+        //route 1 with action type :
+        //[HttpGet("api/cities")] // url : domain.com/api/cities
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<Object>
+                {
+                    new {id = 1, Name ="Tehran"},
+                    new {id = 2, Name ="Esfahan"},
+                    new {id = 3, Name ="Shiraz"}
+                }
+            );
+        }
+    }
+}
